d59ea17f23aecfe94ba870b6933c26a9bd4cb81e
GiacomoDiNardo <vest_95@hotmail.it>
2022-06-07 16:05:22 +0200

    snack 3 con filter

--- snack 3/main.js
+++ snack 3/main.js
@@ -1,15 +1,24 @@
 const myArray = ["Michele", "Fabio", "Roberto", "Giovanni", "Simone", "Chiara"];
 
-let min = 1
-let max = 5
+let min = 1;
+let max = 5;
 
-const namesArray = []
+const namesArray = [];
 
 //Ciclo forEach
 myArray.forEach((nome, i) => {
     if (i > min && i < max) {
-        namesArray.push(nome)
+        namesArray.push(nome);
+    };
+});
+
+console.log(namesArray);
+
+//Con Filter
+const filtroNomi = myArray.filter((nome, i) => {
+    if (i > min && i < max) {
+        return nome;
     }
 })
 
-console.log(namesArray);
+console.log(filtroNomi);
